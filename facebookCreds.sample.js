6a1beb3576582e35d21ec100dd309b5b9c76ff76
Andy Ong <aong93@gmail.com>
2017-11-06 14:45:09 -0800

    edited facebookCreds.sample.js

--- facebookCreds.sample.js
+++ facebookCreds.sample.js
@@ -1,5 +1,5 @@
 module.exports = {
     clientID: "FACEBOOK_APP_ID",
     clientSecret: "FACEBOOK_APP_SECRET",
-    callbackURL: "http://localhost:8000/auth/facebook/callback"
+    callbackURL: "http://localhost:8000/login/facebook/callback"
 }
