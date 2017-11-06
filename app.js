848b0f08d58974e5668998f56862742a3cd37b62
Andy Ong <breathics@Andys-MacBook-Pro.local>
2017-11-06 00:40:41 -0800

    added express-session to test for passport OAuth

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const mysql = require('mysql');
+const session = require('express-session');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mysql = require('mysql');
 
 const app = express();
 const PORT = 8000;
@@ -19,22 +20,22 @@ const pool = mysql.createPool({
 	port: 8889
 })
 
-pool.getConnection(function(err, connection) {
-	console.log("This is the connection");
-	connection.release();
-})
+// pool.getConnection(function(err, connection) {
+// 	console.log("This is the connection");
+// 	connection.release();
+// })
 
-pool.on('acquire', function (connection) {
-  console.log('Connection %d acquired', connection.threadId);
-});
+// pool.on('acquire', function (connection) {
+//   console.log('Connection %d acquired', connection.threadId);
+// });
 
-pool.on('enqueue', function () {
-  console.log('Waiting for available connection slot');
-});
+// pool.on('enqueue', function () {
+//   console.log('Waiting for available connection slot');
+// });
 
-pool.on('release', function (connection) {
-  console.log('Connection %d released', connection.threadId);
-});
+// pool.on('release', function (connection) {
+//   console.log('Connection %d released', connection.threadId);
+// });
 
 
 passport.use(new Strategy(credentials, // First argument accepts an object for clientID, clientSecret, and callbackURL
@@ -51,6 +52,23 @@ passport.use(new Strategy(credentials, // First argument accepts an object for c
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
 
 app.get('/',
 	function(req, res) {
